759e904104c1cbb8dde43aa21d8e8888b59079f9
IsakBD <baglan.issak@gmail.com>
2021-03-15 16:51:29 +0600

    fix the task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,54 @@
-
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+    if (!array) {
+        return 0;
+    } else {
+        if (array.length !== 0) {
+            let min = array[0];
+            for (let i = 1; i < array.length; i++) {
+                if (min > array[i]) {
+                    min = array[i];
+                }
+            }
+            return min;
+        }
+        else{
+            return 0;
+        }
+    }
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+    if (!array) {
+        return 0;
+    } else {
+        if (array.length !== 0) {
+            let max = array[0];
+            for (let i = 1; i < array.length; i++) {
+                if (max < array[i]) {
+                    max = array[i];
+                }
+            }
+            return max;
+        } else {
+            return 0;
+        }
+    }
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+    if (!array) {
+        return 0;
+    } else {
+        if (array.length !== 0) {
+            let sum = 0;
+            for (let i = 0; i < array.length; i++) {
+                sum += array[i];
+            }
+            let avg = sum / array.length;
+            return avg;
+        } else {
+            return 0;
+        }
+    }
 }
+
